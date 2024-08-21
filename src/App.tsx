2ab13651da822f28173a9de4e3e4ac97e1116b9e
Himanish kochhar <himanish.kochhar@gmail.com>
2024-08-21 18:07:10 +0530

    Added the polyfills so we can use bip39 and other packages

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
-
+import { useState } from 'react'
 import './App.css'
-
+import { generateMnemonic } from "bip39";
 import Navbar from './components/Navbar'
 
 function App() {
+  const [mnemonic, setMnemonic] = useState("");
+  console.log(mnemonic)
   return <div className='max-w-7xl mx-auto flex flex-col gap-4 p-4'>
     <Navbar />
 
