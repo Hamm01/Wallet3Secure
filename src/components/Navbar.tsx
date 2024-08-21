bb881f5245c44ebb29195ea53f28edbc066ff36b
Himanish kochhar <himanish.kochhar@gmail.com>
2024-08-21 16:20:00 +0530

    Switch added in ui and Navbar designed

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,17 @@
+
+import { ModeToggle } from '@/components/mode-toggle'
+import { ShieldPlus } from 'lucide-react';;
+
+export default function Navbar() {
+    return (
+        <nav className='max-w-full flex flex-row py-4 justify-between items-center'>
+            <div className="flex items-start gap-2">
+                <h3 className="scroll-m-20 text-4xl font-semibold tracking-tight">
+                    Wallet3Secure
+                </h3>
+                <ShieldPlus size={24} />
+            </div>
+            <ModeToggle />
+        </nav>
+    )
+}
