4b7c689e5568ba619c403cf3717217299c5b9707
Himanish kochhar <himanish.kochhar@gmail.com>
2024-08-22 18:36:51 +0530

    Added footer and packages for crypto wallet

---
+++ src/components/Footer.tsx
@@ -0,0 +1,13 @@
+
+
+export default function Footer() {
+    return (
+        <section className="max-w-7xl mx-auto py-4 border-t ">
+            <div className="flex justify-between">
+                <p className="text-primary text-xl tracking-tight">Developed By &nbsp;
+                    <a href="https://github.com/hamm01">Himanish </a>
+                </p>
+            </div>
+        </section>
+    )
+}
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,7 +4,7 @@ import { ShieldPlus } from 'lucide-react';;
 
 export default function Navbar() {
     return (
-        <nav className='max-w-full flex flex-row py-4 justify-between items-center'>
+        <nav className='flex flex-row py-4 justify-between items-center'>
             <div className="flex items-start gap-2">
                 <h3 className="scroll-m-20 text-4xl font-semibold tracking-tight">
                     Wallet3Secure
