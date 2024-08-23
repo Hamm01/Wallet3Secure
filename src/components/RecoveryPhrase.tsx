ff0e697bd9e41246fbfa515bb4c02e2cc10fd20e
Himanish kochhar <himanish.kochhar@gmail.com>
2024-08-23 18:15:26 +0530

    Wallet home page component and 2nd screen component created

---
+++ src/components/RecoveryPhrase.tsx
@@ -0,0 +1,45 @@
+import React from 'react'
+import { Input } from "@/components/ui/input"
+import { Button } from "@/components/ui/button"
+import {
+    Tooltip,
+    TooltipContent,
+    TooltipProvider,
+    TooltipTrigger,
+} from "@/components/ui/tooltip"
+
+type Props = {
+    walletGenerate: () => void,
+    mnemonicInput: string,
+    setMnemonicInput: React.Dispatch<React.SetStateAction<string>>
+};
+const RecoveryPhrase: React.FC<Props> = ({ walletGenerate, mnemonicInput, setMnemonicInput }) => {
+    return (
+        <div className='flex flex-col gap-4 my-5'>
+            <div className='flex flex-col gap-2'>
+                <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">Wallet Recovery Phrase</h1>
+                <p className="scroll-m-20 pb-2 pt-2 text-2xl font-semibold tracking-tight first:mt-0"> Copy the words at safe place</p>
+            </div>
+            <div className='flex gap-2'>
+                <TooltipProvider>
+                    <Tooltip>
+                        <TooltipTrigger asChild>
+                            <Input type="mnemonics" className='p-5 focus:border-black focus:ring-0 focus:outline-none focus:border-1 ' placeholder="Enter Your Secret Phrase" value={mnemonicInput} onChange={(e) => {
+
+                                return setMnemonicInput(e.target.value)
+                            }} />
+                        </TooltipTrigger>
+                        <TooltipContent side='bottom' sideOffset={20}>
+                            <p className='text-xl'>In case no secret phrase : Leave it blank and press Generate Wallet </p>
+                        </TooltipContent>
+                    </Tooltip>
+                </TooltipProvider>
+
+
+                <Button className='h-10 px-12' onClick={walletGenerate}>Generate Wallet</Button>
+            </div>
+        </div>
+    )
+}
+
+export default RecoveryPhrase
---
+++ src/components/WalletHome.tsx
@@ -0,0 +1,24 @@
+import { Button } from "@/components/ui/button"
+import { ChevronLast } from 'lucide-react'
+type Props = {
+    setPathtypes: React.Dispatch<React.SetStateAction<string[]>>
+};
+const WalletHome: React.FC<Props> = ({ setPathtypes }) => {
+    return (
+        <div className='flex flex-col gap-4'>
+            <div className='flex flex-col gap-2 my-5'>
+                <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">Power Your Crypto Journey: Ethereum & Solana, One Wallet</h1>
+                <p className="scroll-m-20 pb-2 pt-2 text-2xl font-semibold tracking-tight first:mt-0"> Select Blockchain and Get Started</p>
+                <div className='flex gap-2 mt-2'>
+                    <Button size="lg" onClick={() => setPathtypes(["60"])} >Ethereum</Button>
+                    <Button size="lg" onClick={() => setPathtypes(["501"])} >
+                        Solana
+                        <ChevronLast size={20} />
+                    </Button>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default WalletHome
