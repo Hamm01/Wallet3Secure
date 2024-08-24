d7526a0fcc3a2f867ced7905a483df010dad0a94
Himanish kochhar <himanish.kochhar@gmail.com>
2024-08-25 02:04:10 +0530

    Edited the component for Copy to clipboard added with icon

--- src/components/displayRecoveryPhrase.tsx
+++ src/components/displayRecoveryPhrase.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import { ChevronDown, ChevronUp } from 'lucide-react';
+import { ChevronDown, ChevronUp, Copy } from 'lucide-react';
 import { Button } from "@/components/ui/button"
 
 import { toast } from 'sonner';
@@ -18,19 +18,21 @@ export const DisplayRecoveryPhrase: React.FC<RecoveryPhraseType> = ({ mnemonicWo
     return (
         <div className='flex flex-col gap-4'>
             <div className='flex flex-col gap-4 my-10 border p-6 rounded-md'>
-                <div className='content flex justify-between'>
-                    <h1 className="scroll-m-20 text-2xl font-normal" >Secret Phrases</h1>
-                    <Button size="icon" onClick={() => setShowMemonic(!showMemonic)}>{showMemonic ? (<ChevronUp size={20} strokeWidth={1.5} />) : (<ChevronDown size={20} strokeWidth={1.5} />)}</Button>
+                <div className='content flex justify-between cursor-pointer' onClick={() => setShowMemonic(!showMemonic)}>
+                    <h1 className="scroll-m-20 text-2xl font-semibold" >Secret Phrases</h1>
+                    <Button size="icon" variant="ghost" onClick={() => setShowMemonic(!showMemonic)}>{showMemonic ? (<ChevronUp size={20} strokeWidth={1.5} />) : (<ChevronDown size={20} strokeWidth={1.5} />)}</Button>
                 </div>
                 {showMemonic && mnemonicWords.length > 0 && (
-                    <div className="flex flex-col w-full items-center justify-center"
-                        onClick={() => copyToClipboard(mnemonicWords.join(" "))}><div className='grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-2 justify-center w-full items-center mx-auto my-8'>
+                    <div className="flex flex-col w-full justify-center"
+                        onClick={() => copyToClipboard(mnemonicWords.join(" "))}><div className='grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-2 justify-center w-full items-center mx-auto my-4'>
                             {mnemonicWords.map((word, index) => (
                                 <p key={index} className="md:text-lg bg-foreground/5 hover:bg-foreground/10 transition-all duration-300 rounded-lg p-4">{word}
                                 </p>
                             ))}
 
-                        </div></div>)}
+                        </div>
+                        <div className='flex items-center justify-start gap-2 cursor-pointer'> <Button size="icon" variant="ghost" ><Copy size={20} strokeWidth={2} /></Button> <p className='font-semibold'>Copy to clipboard</p></div>
+                    </div>)}
             </div>
         </div>
     )
