f22e0c221dff61c77aad44940a55d35e2c7fbed8
Himanish kochhar <himanish.kochhar@gmail.com>
2024-08-24 15:12:58 +0530

    Added the Sonner library for toast, added the toast functionality

--- src/components/displayRecoveryPhrase.tsx
+++ src/components/displayRecoveryPhrase.tsx
@@ -2,11 +2,19 @@ import { useState } from 'react'
 import { ChevronDown, ChevronUp } from 'lucide-react';
 import { Button } from "@/components/ui/button"
 
+import { toast } from 'sonner';
+
 interface RecoveryPhraseType {
     mnemonicWords: string[]
 }
 export const DisplayRecoveryPhrase: React.FC<RecoveryPhraseType> = ({ mnemonicWords }) => {
     const [showMemonic, setShowMemonic] = useState(false)
+
+
+    const copyToClipboard = (content: string) => {
+        navigator.clipboard.writeText(content)
+        toast('Copied to clipboard!')
+    }
     return (
         <div className='flex flex-col gap-4'>
             <div className='flex flex-col gap-4 my-10 border p-6 rounded-md'>
@@ -14,13 +22,15 @@ export const DisplayRecoveryPhrase: React.FC<RecoveryPhraseType> = ({ mnemonicWo
                     <h1 className="scroll-m-20 text-2xl font-normal" >Secret Phrases</h1>
                     <Button size="icon" onClick={() => setShowMemonic(!showMemonic)}>{showMemonic ? (<ChevronUp size={20} strokeWidth={1.5} />) : (<ChevronDown size={20} strokeWidth={1.5} />)}</Button>
                 </div>
-                {showMemonic && mnemonicWords.length > 0 && (<div className='grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-2 justify-center w-full items-center mx-auto my-8'>
-                    {mnemonicWords.map((word, index) => (
-                        <p key={index} className="md:text-lg bg-foreground/5 hover:bg-foreground/10 transition-all duration-300 rounded-lg p-4">{word}
-                        </p>
-                    ))}
+                {showMemonic && mnemonicWords.length > 0 && (
+                    <div className="flex flex-col w-full items-center justify-center"
+                        onClick={() => copyToClipboard(mnemonicWords.join(" "))}><div className='grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-2 justify-center w-full items-center mx-auto my-8'>
+                            {mnemonicWords.map((word, index) => (
+                                <p key={index} className="md:text-lg bg-foreground/5 hover:bg-foreground/10 transition-all duration-300 rounded-lg p-4">{word}
+                                </p>
+                            ))}
 
-                </div>)}
+                        </div></div>)}
             </div>
         </div>
     )
--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,13 @@ import '../app/globals.css'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { ThemeProvider } from "@/components/theme-provider"
+import { Toaster } from 'sonner';
 import App from './App.tsx'
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
       <App />
+      <Toaster />
     </ThemeProvider>
   </StrictMode>,
 )
